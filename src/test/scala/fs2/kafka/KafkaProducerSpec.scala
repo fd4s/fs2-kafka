a7cc7e55562fde8df71ddf6fd7c837877a792689
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-11-01 18:02:19 +0000

    Add more KafkaProducer tests

--- src/test/scala/fs2/kafka/KafkaProducerSpec.scala
+++ src/test/scala/fs2/kafka/KafkaProducerSpec.scala
@@ -5,7 +5,7 @@ import fs2.{Chunk, Stream}
 import org.apache.kafka.clients.producer.ProducerRecord
 
 final class KafkaProducerSpec extends BaseKafkaSpec {
-  it("should produce all messages") {
+  it("should be able to produce records with single") {
     withKafka { (config, topic) =>
       createCustomTopic(topic, partitions = 3)
       val toProduce = (0 until 100).map(n => s"key-$n" -> s"value->$n")
@@ -27,6 +27,71 @@ final class KafkaProducerSpec extends BaseKafkaSpec {
         consumeNumberKeyedMessagesFrom[String, String](topic, produced.size)
 
       consumed should contain theSameElementsAs produced
+    }
+  }
+
+  it("should be able to produce records with multiple") {
+    withKafka { (config, topic) =>
+      createCustomTopic(topic, partitions = 3)
+      val toProduce = (0 until 10).map(n => s"key-$n" -> s"value->$n").toList
+      val toPassthrough = "passthrough"
+
+      val produced =
+        (for {
+          producer <- producerStream[IO].using(producerSettings(config))
+          records = toProduce.map {
+            case (key, value) =>
+              new ProducerRecord(topic, key, value)
+          }
+          message = ProducerMessage.multiple(records, toPassthrough)
+          result <- Stream.eval(producer.produce(message))
+        } yield result).compile.lastOrError.unsafeRunSync
+
+      produced match {
+        case ProducerResult.Multiple(parts, passthrough) =>
+          val produced = parts.map(part => (part.record.key, part.record.value))
+          assert(produced == toProduce && passthrough == toPassthrough)
+
+        case result =>
+          fail(s"unexpected producer result: $result")
+      }
+
+      val consumed =
+        consumeNumberKeyedMessagesFrom[String, String](topic, toProduce.size)
+
+      consumed should contain theSameElementsAs toProduce
+    }
+  }
+
+  it("should be able to produce zero records with multiple") {
+    withKafka { (config, topic) =>
+      createCustomTopic(topic, partitions = 3)
+      val passthrough = "passthrough"
+
+      val result =
+        (for {
+          producer <- producerStream[IO].using(producerSettings(config))
+          records = List.empty[ProducerRecord[String, String]]
+          message = ProducerMessage.multiple(records, passthrough)
+          result <- Stream.eval(producer.produce(message))
+        } yield result).compile.lastOrError.unsafeRunSync
+
+      assert(result.passthrough == passthrough)
+    }
+  }
+
+  it("should be able to produce zero records with passthrough") {
+    withKafka { (config, topic) =>
+      createCustomTopic(topic, partitions = 3)
+      val passthrough = "passthrough"
+
+      val result =
+        (for {
+          producer <- producerStream[IO].using(producerSettings(config))
+          result <- Stream.eval(producer.produce(ProducerMessage.passthrough(passthrough)))
+        } yield result).compile.lastOrError.unsafeRunSync
+
+      assert(result.passthrough == passthrough)
     }
   }
 }
