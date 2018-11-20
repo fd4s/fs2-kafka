d006dbc51d2057cc0fe14cc500e1edc0b2a30072
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-11-20 15:34:27 +0000

    Change to improve performance of offset batching

--- src/main/scala/fs2/kafka/CommittableOffset.scala
+++ src/main/scala/fs2/kafka/CommittableOffset.scala
@@ -22,19 +22,71 @@ import fs2.kafka.internal.instances._
 import org.apache.kafka.clients.consumer.OffsetAndMetadata
 import org.apache.kafka.common.TopicPartition
 
+/**
+  * [[CommittableOffset]] represents an [[offsetAndMetadata]] for a
+  * [[topicPartition]], along with the ability to commit that offset
+  * to Kafka with [[commit]]. Note that offsets are normally committed
+  * in batches for performance reasons. Sinks like [[commitBatch]] and
+  * [[commitBatchWithin]] use [[CommittableOffsetBatch]] to commit the
+  * offsets in batches.<br>
+  * <br>
+  * While normally not necessary, [[CommittableOffset#apply]] can be
+  * used to create a new instance.
+  */
 sealed abstract class CommittableOffset[F[_]] {
+
+  /**
+    * The topic and partition for which [[offsetAndMetadata]]
+    * can be committed using [[commit]].
+    */
   def topicPartition: TopicPartition
 
+  /**
+    * The offset and metadata for the [[topicPartition]], which
+    * can be committed using [[commit]].
+    */
   def offsetAndMetadata: OffsetAndMetadata
 
+  /**
+    * The [[topicPartition]] and [[offsetAndMetadata]] as a `Map`.
+    * This is provided for convenience and is always guaranteed to
+    * be equivalent to the following.
+    *
+    * {{{
+    * Map(topicPartition -> offsetAndMetadata)
+    * }}}
+    */
   def offsets: Map[TopicPartition, OffsetAndMetadata]
 
+  /**
+    * The [[CommittableOffset]] as a [[CommittableOffsetBatch]].
+    */
   def batch: CommittableOffsetBatch[F]
 
+  /**
+    * Commits the [[offsetAndMetadata]] for the [[topicPartition]] to
+    * Kafka. Note that offsets are normally committed in batches for
+    * performance reasons. Prefer to use sinks like [[commitBatch]]
+    * or [[commitBatchWithin]], or [[CommittableOffsetBatch]] for
+    * that reason.
+    */
   def commit: F[Unit]
+
+  /**
+    * The commit function we are using in [[commit]] to commit the
+    * [[offsetAndMetadata]] for the [[topicPartition]]. Is used to
+    * help achieve better performance when batching offsets.
+    */
+  private[kafka] def commitOffsets: Map[TopicPartition, OffsetAndMetadata] => F[Unit]
 }
 
 object CommittableOffset {
+
+  /**
+    * Creates a new [[CommittableOffset]] with the specified `topicPartition`
+    * and `offsetAndMetadata`, along with `commit`, describing how to commit
+    * an arbitrary `Map` of topic-partition offsets.
+    */
   def apply[F[_]](
     topicPartition: TopicPartition,
     offsetAndMetadata: OffsetAndMetadata,
@@ -51,7 +103,7 @@ object CommittableOffset {
       override val offsetAndMetadata: OffsetAndMetadata =
         _offsetAndMetadata
 
-      override val offsets: Map[TopicPartition, OffsetAndMetadata] =
+      override def offsets: Map[TopicPartition, OffsetAndMetadata] =
         Map(_topicPartition -> _offsetAndMetadata)
 
       override def batch: CommittableOffsetBatch[F] =
@@ -59,6 +111,9 @@ object CommittableOffset {
 
       override def commit: F[Unit] =
         _commit(offsets)
+
+      override val commitOffsets: Map[TopicPartition, OffsetAndMetadata] => F[Unit] =
+        _commit
 
       override def toString: String =
         Show[CommittableOffset[F]].show(this)
