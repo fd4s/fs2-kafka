ef56ce2a7f7e998d63b9115754730e2fcda40ecf
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-11-21 14:41:26 +0000

    Add documentation for ProducerMessage

--- src/main/scala/fs2/kafka/ProducerMessage.scala
+++ src/main/scala/fs2/kafka/ProducerMessage.scala
@@ -21,6 +21,27 @@ import cats.syntax.show._
 import fs2.kafka.internal.instances._
 import org.apache.kafka.clients.producer.ProducerRecord
 
+/**
+  * [[ProducerMessage]] represents zero or more `ProducerRecord`s
+  * together with an arbitrary passthrough value, which together
+  * can be used with [[KafkaProducer]]. A [[ProducerMessage]] can
+  * be created using one of the following options.<br>
+  * <br>
+  * - [[ProducerMessage#single]] to produce exactly one record and
+  * then emit a [[ProducerResult#Single]] with the result and the
+  * passthrough value.<br>
+  * - [[ProducerMessage#multiple]] to produce zero or more records
+  * and then emit a [[ProducerResult#Multiple]] with the results
+  * and the passthrough value.<br>
+  * - [[ProducerMessage#passthrough]] to produce exactly zero records,
+  * simply emitting a [[ProducerResult#Passthrough]] with the specified
+  * passthrough value.<br>
+  * <br>
+  * While normally not necessary, the passthrough of a [[ProducerMessage]]
+  * can be accessed via [[passthrough]]. There are also extractors for the
+  * three cases: [[ProducerMessage#Single]], [[ProducerMessage#Multiple]],
+  * and [[ProducerMessage#Passthrough]].
+  */
 sealed abstract class ProducerMessage[K, V, P] {
   def passthrough: P
 }
@@ -77,18 +98,42 @@ object ProducerMessage {
     }
   }
 
+  /**
+    * Creates a new [[ProducerMessage]] for producing exactly one
+    * `ProducerRecord`, then emitting a [[ProducerResult#Single]]
+    * with the result and specified passthrough value.<br>
+    * <br>
+    * [[ProducerMessage#Single]] can be used to extract instances
+    * created with this function.
+    */
   def single[K, V, P](
     record: ProducerRecord[K, V],
     passthrough: P
   ): ProducerMessage[K, V, P] =
     new Single(record, passthrough) {}
 
+  /**
+    * Creates a new [[ProducerMessage]] for producing zero or more
+    * `ProducerRecords`s, then emitting a [[ProducerResult#Multiple]]
+    * with the results and specified passthrough value.<br>
+    * <br>
+    * [[ProducerMessage#Multiple]] can be used to extract instances
+    * created with this function.
+    */
   def multiple[K, V, P](
     records: List[ProducerRecord[K, V]],
     passthrough: P
   ): ProducerMessage[K, V, P] =
     new Multiple(records, passthrough) {}
 
+  /**
+    * Creates a new [[ProducerMessage]] for producing exactly zero
+    * `ProducerRecord`s, emitting a [[ProducerResult#passthrough]]
+    * with the specified passthrough value.<br>
+    * <br>
+    * [[ProducerMessage#Passthrough]] can be used to extract instances
+    * created with this function.
+    */
   def passthrough[K, V, P](
     passthrough: P
   ): ProducerMessage[K, V, P] =
