f2675887f62a8d55623b401685d9e600641a8775
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-11-21 14:41:11 +0000

    Add documentation for ProducerFactory

--- src/main/scala/fs2/kafka/ProducerFactory.scala
+++ src/main/scala/fs2/kafka/ProducerFactory.scala
@@ -21,6 +21,22 @@ import org.apache.kafka.clients.producer.Producer
 
 import scala.collection.JavaConverters._
 
+/**
+  * [[ProducerFactory]] represents the ability to create a
+  * new Kafka `Producer` given [[ProducerSettings]]. Normal
+  * usage does not require a custom [[ProducerFactory]], but
+  * it can be useful for testing purposes.<br>
+  * <br>
+  * To create a new [[ProducerFactory]], simply create a new
+  * instance and implement the [[create]] function with the
+  * desired `Producer` behaviour. To use a custom instance
+  * of [[ProducerFactory]], you can simply set it with the
+  * [[ProducerSettings#withProducerFactory]] function.<br>
+  * <br>
+  * [[ProducerFactory#Default]] is the default instance, and
+  * it creates a default `KafkaProducer` instance from the
+  * provided [[ProducerSettings]].
+  */
 abstract class ProducerFactory {
   def create[F[_], K, V](
     settings: ProducerSettings[K, V]
@@ -28,6 +44,12 @@ abstract class ProducerFactory {
 }
 
 object ProducerFactory {
+
+  /**
+    * The default [[ProducerFactory]] used in [[ProducerSettings]]
+    * unless a different one has been specified. Default instance
+    * creates `KafkaProducer` instances from provided settings.
+    */
   val Default: ProducerFactory =
     new ProducerFactory {
       override def create[F[_], K, V](
