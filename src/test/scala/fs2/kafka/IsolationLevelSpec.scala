53ecc98e5819d0041eb88c5d6fcdc3399b5aac2b
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2019-05-15 20:13:58 +0100

    Add IsolationLevel tests

--- src/test/scala/fs2/kafka/IsolationLevelSpec.scala
+++ src/test/scala/fs2/kafka/IsolationLevelSpec.scala
@@ -2,7 +2,7 @@ package fs2.kafka
 
 final class IsolationLevelSpec extends BaseSpec {
   describe("IsolationLevel") {
-    it("should have expected toString") {
+    it("should have toString matching name") {
       assert(IsolationLevel.ReadCommitted.toString == "ReadCommitted")
       assert(IsolationLevel.ReadUncommitted.toString == "ReadUncommitted")
     }
