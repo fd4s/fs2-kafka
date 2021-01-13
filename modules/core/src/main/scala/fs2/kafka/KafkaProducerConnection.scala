39a095bd59588990b2c9dce776e056da9f791617
Ben Plommer <ben.plommer@itv.com>
2021-01-13 19:28:29 +0000

    add scaladocs for KafkaProducerConnection

--- modules/core/src/main/scala/fs2/kafka/KafkaProducerConnection.scala
+++ modules/core/src/main/scala/fs2/kafka/KafkaProducerConnection.scala
@@ -11,18 +11,49 @@ import cats.implicits._
 import fs2.Stream
 import fs2.kafka.internal._
 
+/**
+  * [[KafkaProducerConnection]] represents a connection to a Kafka broker
+  * that can be used to create [[KafkaProducer]] instances. All [[KafkaProducer]]
+  * instances created from an given [[KafkaProducerConnection]] share a single
+  * underlying connection.
+  */
 sealed abstract class KafkaProducerConnection[F[_]] {
+
+  /**
+    * Creates a new [[KafkaProducer]]  using the provided serializers.
+    *
+    * {{{
+    * KafkaProducerConnection.stream[F].using(settings).map(_.withSerializers(keySerializer, valueSerializer))
+    * }}}
+    */
   def withSerializers[K, V](
     keySerializer: Serializer[F, K],
     valueSerializer: Serializer[F, V]
   ): KafkaProducer.Metrics[F, K, V]
 
+  /**
+    * Creates a new [[KafkaProducer]] in the `F` context,
+    * using serializers from the specified [[ProducerSettings]].
+    *
+    * {{{
+    * KafkaProducerConnection.stream[F].using(settings).evalMap(_.withSerializersFrom(settings))
+    * }}}
+    */
   def withSerializersFrom[K, V](
     settings: ProducerSettings[F, K, V]
   ): F[KafkaProducer.Metrics[F, K, V]]
 }
 
 object KafkaProducerConnection {
+
+  /**
+    * Creates a new [[KafkaProducerConnection]] in the `Stream` context,
+    * using the specified [[ProducerSettings]].
+    *
+    * {{{
+    * KafkaProducerConnection.stream[F].using(settings)
+    * }}}
+    */
   def stream[F[_]](
     settings: ProducerSettings[F, _, _]
   )(
@@ -30,6 +61,14 @@ object KafkaProducerConnection {
     context: ContextShift[F]
   ): Stream[F, KafkaProducerConnection[F]] = Stream.resource(resource(settings))
 
+  /**
+    * Creates a new [[KafkaProducerConnection]] in the `Resource` context,
+    * using the specified [[ProducerSettings]].
+    *
+    * {{{
+    * KafkaProducerConnection.resource[F].using(settings)
+    * }}}
+    */
   def resource[F[_]](
     settings: ProducerSettings[F, _, _]
   )(
