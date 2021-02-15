64c41f2969dcaa191afefa924a46de2452359c3a
Ben Plommer <ben.plommer@itv.com>
2021-02-15 17:43:48 +0000

    fix merge mistake

--- modules/core/src/main/scala/fs2/kafka/consumer/KafkaConsumerLifecycle.scala
+++ modules/core/src/main/scala/fs2/kafka/consumer/KafkaConsumerLifecycle.scala
@@ -12,14 +12,17 @@ trait KafkaConsumerLifecycle[F[_]] {
     * Whenever `terminate` is invoked, an attempt will be made to stop the
     * underlying consumer. The `terminate` operation will not wait for the
     * consumer to shutdown. If you also want to wait for the shutdown
-    * to complete, you can use `awaitTermination`.<br>
+    * to complete, you can use `terminate >> awaitTermination`.<br>
     */
   def terminate: F[Unit]
 
   /**
-    * Shutdown and wait for it to complete. Note that `awaitTermination` is guaranteed
+    * Wait for consumer to shut down. Note that `awaitTermination` is guaranteed
     * to complete after consumer shutdown, even when the consumer is
-    * cancelled with `terminate`.<br>
+    * cancelled with `terminate`.
+    *
+    * This method will not initiate shutdown. To initiate shutdown and wait for
+    * it to complete, you can use `terminate >> awaitTermination`.
     */
   def awaitTermination: F[Unit]
 }
