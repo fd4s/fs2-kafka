b1f0e81962a2b3f4550f204d51950ec8b6cf7556
Ben Plommer <ben.plommer@itv.com>
2021-04-06 08:06:51 +0100

    Add docs

--- modules/core/src/main/scala/fs2/kafka/admin/MkAdminClient.scala
+++ modules/core/src/main/scala/fs2/kafka/admin/MkAdminClient.scala
@@ -8,9 +8,20 @@ package fs2.kafka.admin
 
 import cats.effect.Sync
 import fs2.kafka.AdminClientSettings
+import fs2.kafka.consumer.MkConsumer
 import org.apache.kafka.clients.admin.AdminClient
 import fs2.kafka.internal.converters.collection._
 
+/**
+  * A capability trait representing the ability to instantiate the Java
+  * `AdminClient` that underlies the fs2-kafka `KafkaAdminClient`. This
+  * is needed in order to instantiate [[fs2.kafka.KafkaAdminClient]].
+  *
+  * By default, the instance provided by [[MkAdminClient.mkAdminClientForSync]]
+  * will be used. However this behaviour can be overridden, e.g. for
+  * testing purposes, by placing an alternative implicit instance in
+  * lexical scope.
+  */
 trait MkAdminClient[F[_]] {
   def apply(settings: AdminClientSettings): F[AdminClient]
 }
