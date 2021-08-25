45f395ca6f1ef4a44c3b9fa277de1205fcdb48fa
Keir Lawson <keirlawson@gmail.com>
2021-08-25 16:12:29 +0100

    Add docs for munit module

--- docs/src/main/scala/fs2/kafka/docs/Main.scala
+++ docs/src/main/scala/fs2/kafka/docs/Main.scala
@@ -37,6 +37,7 @@ object Main {
           "CORE_CROSS_SCALA_VERSIONS" -> minorVersionsString(coreCrossScalaVersions),
           "VULCAN_MODULE_NAME" -> vulcanModuleName,
           "VULCAN_CROSS_SCALA_VERSIONS" -> minorVersionsString(vulcanCrossScalaVersions),
+          "VULCAN_TESTKIT_MUNIT_MODULE_NAME" -> vulcanTeskkitMunitModuleName,
           "LATEST_VERSION" -> latestVersion,
           "LATEST_SNAPSHOT_VERSION" -> latestSnapshotVersion,
           "LATEST_MAJOR_VERSION" -> majorVersion(latestVersion),
