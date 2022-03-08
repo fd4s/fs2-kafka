fdd372265ea210bcc953dd6e0119db201e5dc9ab
Ben Plommer <ben.plommer@gmail.com>
2022-03-08 10:30:51 +0000

    Update modules/core/src/main/scala/fs2/kafka/internal/package.scala

--- modules/core/src/main/scala/fs2/kafka/internal/package.scala
+++ modules/core/src/main/scala/fs2/kafka/internal/package.scala
@@ -8,8 +8,8 @@ package fs2.kafka
 
 import cats.effect.{Blocker, ContextShift, Resource, Sync}
 
-package object internal {
-  type ExclusiveAccess[F[_], A] = F[A] => F[A]
+private[kafka] package object internal {
+  private[kafka] type ExclusiveAccess[F[_], A] = F[A] => F[A]
 
   private[kafka] def blockingResource[F[_]: Sync: ContextShift](
     settings: ProducerSettings[F, _, _]
