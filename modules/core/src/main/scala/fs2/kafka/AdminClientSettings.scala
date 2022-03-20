8d6f22762b26986e9c91b2b1eb51d5727dd6e5fc
Ben Plommer <ben.plommer@gmail.com>
2022-03-20 13:29:12 +0000

    Use KafkaClientSettings in AdminClientSettings

--- modules/core/src/main/scala/fs2/kafka/AdminClientSettings.scala
+++ modules/core/src/main/scala/fs2/kafka/AdminClientSettings.scala
@@ -24,36 +24,7 @@ import scala.concurrent.duration._
   * Use [[AdminClientSettings#apply]] for the default settings, and
   * then apply any desired modifications on top of that instance.
   */
-sealed abstract class AdminClientSettings {
-
-  /**
-    * Properties which can be provided when creating a Java `KafkaAdminClient`
-    * instance. Numerous functions in [[AdminClientSettings]] add properties
-    * here if the settings are used by the Java `KafkaAdminClient`.
-    */
-  def properties: Map[String, String]
-
-  /**
-    * Returns a new [[AdminClientSettings]] instance with the specified
-    * bootstrap servers. This is equivalent to setting the following
-    * property using the [[withProperty]] function.
-    *
-    * {{{
-    * AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG
-    * }}}
-    */
-  def withBootstrapServers(bootstrapServers: String): AdminClientSettings
-
-  /**
-    * Returns a new [[AdminClientSettings]] instance with the specified
-    * client id. This is equivalent to setting the following property
-    * using the [[withProperty]] function.
-    *
-    * {{{
-    * AdminClientConfig.CLIENT_ID_CONFIG
-    * }}}
-    */
-  def withClientId(clientId: String): AdminClientSettings
+sealed abstract class AdminClientSettings extends KafkaClientSettings[AdminClientSettings] {
 
   /**
     * Returns a new [[AdminClientSettings]] instance with the specified
@@ -105,18 +76,6 @@ sealed abstract class AdminClientSettings {
 
   /**
     * Returns a new [[AdminClientSettings]] instance with the specified
-    * request timeout. This is equivalent to setting the following
-    * property using the [[withProperty]] function, except you can
-    * specify it with a `FiniteDuration` instead of a `String`.
-    *
-    * {{{
-    * AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG
-    * }}}
-    */
-  def withRequestTimeout(requestTimeout: FiniteDuration): AdminClientSettings
-
-  /**
-    * Returns a new [[AdminClientSettings]] instance with the specified
     * max metadata age. This is equivalent to setting the following
     * property using the [[withProperty]] function, except you can
     * specify it with a `FiniteDuration` instead of a `String`.
@@ -138,44 +97,6 @@ sealed abstract class AdminClientSettings {
     * }}}
     */
   def withRetries(retries: Int): AdminClientSettings
-
-  /**
-    * Includes a property with the specified `key` and `value`.
-    * The key should be one of the keys in `AdminClientConfig`,
-    * and the value should be a valid choice for the key.
-    */
-  def withProperty(key: String, value: String): AdminClientSettings
-
-  /**
-    * Includes the specified keys and values as properties. The
-    * keys should be part of the `AdminClientConfig` keys, and
-    * the values should be valid choices for the keys.
-    */
-  def withProperties(properties: (String, String)*): AdminClientSettings
-
-  /**
-    * Includes the specified keys and values as properties. The
-    * keys should be part of the `AdminClientConfig` keys, and
-    * the values should be valid choices for the keys.
-    */
-  def withProperties(properties: Map[String, String]): AdminClientSettings
-
-  /**
-    * The time to wait for the Java `KafkaAdminClient` to shutdown.<br>
-    * <br>
-    * The default value is 20 seconds.
-    */
-  def closeTimeout: FiniteDuration
-
-  /**
-    * Creates a new [[AdminClientSettings]] with the specified [[closeTimeout]].
-    */
-  def withCloseTimeout(closeTimeout: FiniteDuration): AdminClientSettings
-
-  /**
-    * Includes the credentials properties from the provided [[KafkaCredentialStore]]
-    */
-  def withCredentials(credentialsStore: KafkaCredentialStore): AdminClientSettings
 }
 
 object AdminClientSettings {
@@ -183,12 +104,6 @@ object AdminClientSettings {
     override val properties: Map[String, String],
     override val closeTimeout: FiniteDuration
   ) extends AdminClientSettings {
-
-    override def withBootstrapServers(bootstrapServers: String): AdminClientSettings =
-      withProperty(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
-
-    override def withClientId(clientId: String): AdminClientSettings =
-      withProperty(AdminClientConfig.CLIENT_ID_CONFIG, clientId)
 
     override def withReconnectBackoff(reconnectBackoff: FiniteDuration): AdminClientSettings =
       withProperty(
@@ -215,29 +130,17 @@ object AdminClientSettings {
         connectionsMaxIdle.toMillis.toString
       )
 
-    override def withRequestTimeout(requestTimeout: FiniteDuration): AdminClientSettings =
-      withProperty(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout.toMillis.toString)
-
     override def withMetadataMaxAge(metadataMaxAge: FiniteDuration): AdminClientSettings =
       withProperty(AdminClientConfig.METADATA_MAX_AGE_CONFIG, metadataMaxAge.toMillis.toString)
 
     override def withRetries(retries: Int): AdminClientSettings =
       withProperty(AdminClientConfig.RETRIES_CONFIG, retries.toString)
 
-    override def withProperty(key: String, value: String): AdminClientSettings =
-      copy(properties = properties.updated(key, value))
-
-    override def withProperties(properties: (String, String)*): AdminClientSettings =
-      copy(properties = this.properties ++ properties.toMap)
-
     override def withProperties(properties: Map[String, String]): AdminClientSettings =
       copy(properties = this.properties ++ properties)
 
     override def withCloseTimeout(closeTimeout: FiniteDuration): AdminClientSettings =
       copy(closeTimeout = closeTimeout)
-
-    override def withCredentials(credentialsStore: KafkaCredentialStore): AdminClientSettings =
-      withProperties(credentialsStore.properties)
 
     override def toString: String =
       s"AdminClientSettings(closeTimeout = $closeTimeout)"
