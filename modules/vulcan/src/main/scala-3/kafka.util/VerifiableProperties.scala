6d268b4020deaef162db3637645de470e147e788
Ben Plommer <ben.plommer@gmail.com>
2022-03-31 18:18:40 +0100

    Scala 3 bug workaround

---
+++ modules/vulcan/src/main/scala-3/kafka.util/VerifiableProperties.scala
@@ -0,0 +1,5 @@
+package kafka.utils
+
+/* Workaround for https://github.com/lampepfl/dotty/issues/13523 and
+    https://github.com/confluentinc/schema-registry/issues/553 */
+private class VerifiableProperties
