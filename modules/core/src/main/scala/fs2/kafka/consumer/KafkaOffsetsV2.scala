cb3406551ea9420b604402b45dcb4e7c86d50741
dmedser <d.e.serebryansky@gmail.com>
2022-03-31 15:57:41 +0300

    header for KafkaOffsetsV2

--- modules/core/src/main/scala/fs2/kafka/consumer/KafkaOffsetsV2.scala
+++ modules/core/src/main/scala/fs2/kafka/consumer/KafkaOffsetsV2.scala
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2018-2022 OVO Energy Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package fs2.kafka.consumer
 
 import org.apache.kafka.clients.consumer.OffsetAndMetadata
