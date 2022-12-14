18cb464d5cebce7b3d532d4cd6af3ddb96f6bc60
Ben Stewart <benalanstewart@gmail.com>
2022-12-14 16:12:14 +0000

    Change `Blocking` to use `Sync#interruptible`, not `blocking`
    This is to avoid being unable to cancel code that is inside blocking
    section. At the moment it is possible for user-code to not cancel
    immediately, in which case it will block until an internal Kafka
    max-block period is reached (by default 1 minute).
    
    Changing to use `interruptible` means the user-code returns immediately
    on cancellation, and the underlying operation in Kafka should be
    cancelled.
    
    As a concrete example, at the moment producing to a topic which does not
    exist blocks until a minute has passed, even if the effect is cancelled.
    For example:
    
    ```scala
    type K
    type V
    
    val producer: TransactionalKafkaProducer.WithoutOffsets[IO, Option[K], Option[V]] = ???
    
    producer
      .produceWithoutOffsets(
        ProducerRecords.one(ProducerRecord(topic = "does_not_exist", key = None, value = None))
      )
      .timeout(1.second)
      .unsafeRunSync()
    ```
    
    Changing to use `interruptible` means it completes after 1 second with
    the timeout exception.

--- modules/core/src/main/scala/fs2/kafka/internal/Blocking.scala
+++ modules/core/src/main/scala/fs2/kafka/internal/Blocking.scala
@@ -18,7 +18,7 @@ private[kafka] trait Blocking[F[_]] {
 
 private[kafka] object Blocking {
   def fromSync[F[_]: Sync]: Blocking[F] = new Blocking[F] {
-    override def apply[A](a: => A): F[A] = Sync[F].blocking(a)
+    override def apply[A](a: => A): F[A] = Sync[F].interruptible(a)
   }
 
   def fromExecutionContext[F[_]](ec: ExecutionContext)(implicit F: Async[F]): Blocking[F] =
