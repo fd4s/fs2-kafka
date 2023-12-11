2a241caacd440495b78aec8af0b5c5e01644861f
Leonhard Riedisser <leo008180@googlemail.com>
2023-12-11 15:14:24 +0100

    Provide a single `consumeChunk` method instead of mirroring the full consumer API

--- modules/core/src/main/scala/fs2/kafka/consumer/KafkaConsumeChunk.scala
+++ modules/core/src/main/scala/fs2/kafka/consumer/KafkaConsumeChunk.scala
@@ -1,51 +1,41 @@
 package fs2.kafka.consumer
 
+import cats.effect.Concurrent
 import cats.syntax.flatMap.*
-import cats.MonadThrow
+import cats.Monad
 import fs2.*
 import fs2.kafka.consumer.KafkaConsumeChunk.CommitNow
 import fs2.kafka.CommittableConsumerRecord
 import fs2.kafka.CommittableOffsetBatch
 import fs2.kafka.ConsumerRecord
 
-import org.apache.kafka.common.TopicPartition
-
 trait KafkaConsumeChunk[F[_], K, V] extends KafkaConsume[F, K, V] {
 
-  final def recordsChunk(
-    processor: Chunk[ConsumerRecord[K, V]] => F[CommitNow.type]
-  )(implicit F: MonadThrow[F]): Stream[F, Unit] = streamChunk(processor)
+  final def consumeChunk(
+    processor: Chunk[ConsumerRecord[K, V]] => F[CommitNow]
+  )(implicit F: Concurrent[F]): F[Nothing] = partitionedStream
+    .map(
+      _.chunks.evalMap(consumeChunk(processor))
+    )
+    .parJoinUnbounded
+    .compile
+    .drain >> F.never
 
-  final def streamChunk(
-    processor: Chunk[ConsumerRecord[K, V]] => F[CommitNow.type]
-  )(implicit F: MonadThrow[F]): Stream[F, Unit] =
-    stream.chunks.evalMap(consumeChunk(processor))
-
-  final def partitionedRecordsChunk(
-    processor: Chunk[ConsumerRecord[K, V]] => F[CommitNow.type]
-  )(implicit F: MonadThrow[F]): Stream[F, Stream[F, Unit]] =
-    partitionedStreamChunk(processor)
-
-  final def partitionedStreamChunk(
-    processor: Chunk[ConsumerRecord[K, V]] => F[CommitNow.type]
-  )(implicit F: MonadThrow[F]): Stream[F, Stream[F, Unit]] =
-    partitionedStream.map(_.chunks.evalMap(consumeChunk(processor)))
-
-  final def partitionsMapStreamChunk(
-    processor: Chunk[ConsumerRecord[K, V]] => F[CommitNow.type]
-  )(implicit F: MonadThrow[F]): Stream[F, Map[TopicPartition, Stream[F, Unit]]] =
-    partitionsMapStream
-      .map(m => m.map(kv => (kv._1, kv._2.chunks.evalMap(consumeChunk(processor)))))
-
-  private def consumeChunk(processor: Chunk[ConsumerRecord[K, V]] => F[CommitNow.type])(
+  private def consumeChunk(processor: Chunk[ConsumerRecord[K, V]] => F[CommitNow])(
     chunk: Chunk[CommittableConsumerRecord[F, K, V]]
-  )(implicit F: MonadThrow[F]): F[Unit] =
-    processor(chunk.map(_.record)) >> CommittableOffsetBatch
-      .fromFoldable(chunk.map(_.offset))
-      .commit
+  )(implicit F: Monad[F]): F[Unit] = {
+    val (offsets, records) = chunk.foldLeft(
+      (CommittableOffsetBatch.empty, Chunk.empty[ConsumerRecord[K, V]])
+    )((acc, record) => (acc._1.updated(record.offset), acc._2 ++ Chunk(record.record)))
+
+    processor(records) >> offsets.commit
+  }
 
 }
 
 object KafkaConsumeChunk {
+
+  type CommitNow = CommitNow.type
   object CommitNow
+
 }
