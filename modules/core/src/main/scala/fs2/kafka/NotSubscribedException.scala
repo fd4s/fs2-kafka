5747be62bf42218cfe6b26a5875e67843856d64d
Alan Artigao Carreño <alanartigao@gmail.com>
2024-03-04 07:33:58 +0100

    Improve `NotSubscribedException` message (#1303)

--- modules/core/src/main/scala/fs2/kafka/NotSubscribedException.scala
+++ modules/core/src/main/scala/fs2/kafka/NotSubscribedException.scala
@@ -10,10 +10,11 @@ import org.apache.kafka.common.KafkaException
 
 /**
   * [[NotSubscribedException]] indicates that a `Stream` was started in [[KafkaConsumer]] even
-  * though the consumer had not been subscribed to any topics before starting.
+  * though the consumer had not been subscribed to any topics or assigned any partitions before
+  * starting.
   */
 sealed abstract class NotSubscribedException
-    extends KafkaException("consumer is not subscribed to any topics")
+    extends KafkaException("consumer is not subscribed to any topics or assigned any partitions")
 
 private[kafka] object NotSubscribedException {
 
